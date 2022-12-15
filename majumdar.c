3c1cf2ab57d6a1e4c1bea82b34fa893a12e2385b
hasanabill <hasanabilna@gmail.com>
2022-12-15 17:04:39 +0600

    code organized a little

--- majumdar.c
+++ majumdar.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 // functin declaration
 void menu();
 int auth();
 void submenu();
-void projectMenu();
-void addNewProject();
-void saveProjectInfo();
-void viewAllProjectInfo();
+void manageProject();
+void createProject();
+void saveProject();
+void viewAllProject();
+void updateProject();
+void deleteProject();
 
+// defining ftructure
+struct Projects
+{
+    int id;
+    char projectName[50];
+    char projectType[10];
+    char description[100];
+    char startDate[15];
+    char endDate[15];
+    int estBudget;
+};
+
+// declaring file pointer for CRUD operation
+FILE *filetooperate;
+// main function of the program
 int main()
 {
 
@@ -27,16 +45,18 @@ login:
     return 0;
 }
 
+// function for main menu
 void menu()
 {
     system("cls");
-    printf("\n\n\n **** Mazumdar's Agro & Frishries Admin Panel. **** \n   1. Manage Projects  \n   2. Manage Purchases.\n   2. Manage Feeding History. \n   4. Staffs. \n   5. Back to home page.\n   6. Exit.");
+    printf("\n\n\n **** Mazumdar's Agro & Frishries Admin Panel. **** \n   1. Manage Projects  \n   2. Manage Purchases.\n   2. Manage Feeding History. \n   4. Staffs. \n   5. Back to home page.\n   0. Exit.");
     printf("\n\nChoose the options what you want to do (1/2/3/4/5/6): ");
 
     int userChoice;
 
     scanf("%d", &userChoice);
 
+menuinput:
     switch (userChoice)
     {
     case 1:
@@ -52,14 +72,15 @@ void menu()
         submenu();
         break;
     case 5:
-        submenu();
+        menu();
         break;
-    case 6:
+    case 0:
         submenu();
         break;
 
     default:
-        printf("Invalid choice");
+        printf("Invalid choice. Please enter valid choice:");
+        goto menuinput;
     }
 }
 
@@ -79,33 +100,44 @@ int auth()
 
 void manageProject()
 {
-    printf("****** Sub Menu ******\n");
+
+    //
+    printf("\n\n****** Mazumdar's Agro & Fisheries Admin Panel ******\n");
     printf("1. Create new project\n");
     printf("2. View All Projects\n");
     printf("3. Update your project\n");
     printf("4. Delete your project\n");
-    printf("5. Back to Main Menu\n");
-
+    printf("0. Back to Main Menu\n");
     int userChoice;
+
+    // user input
+    printf("Choose: ");
+
+subinput:
+    fflush(stdin);
     scanf("%d", &userChoice);
 
     switch (userChoice)
     {
     case 1:
-        manageProject();
+        createProject();
         break;
     case 2:
-        submenu();
+        viewAllProject();
         break;
     case 3:
-        submenu();
+        updateProject();
         break;
     case 4:
-        submenu();
+        deleteProject();
+        break;
+    case 0:
+        menu();
         break;
 
     default:
-        printf("Invalid choice");
+        printf("Invalid choice! \nEnter valid choice: ");
+        goto subinput;
     }
 }
 void submenu()
@@ -127,54 +159,3 @@ void submenu()
         printf("you choose 2");
     }
 }
-
-struct manage_project
-{
-    char code[10];
-    char name[30];
-    char type[10];
-    char description[50];
-    char start_date[10];
-    char end_date[10];
-    float est_budget;
-};
-
-struct manage_Expenses
-{
-    char _id[10];
-    char date[30];
-    char type[12];
-    char product_or_service[50];
-    char supplier_info[25];
-    float amount;
-    char project_id[10];
-};
-
-struct feed_history
-{
-    char project_id[10];
-    char date[10];
-    char time[10];
-    float quantity;
-    char food_category[30];
-};
-
-struct selling
-{
-    char _id[10];
-    char date[10];
-    char buyer[30];
-    float quantity;
-    float unit_price;
-    float total_price;
-};
-
-struct employee
-{
-    char _id[10];
-    char name[30];
-    char address[50];
-    int phone_number;
-    char email[25];
-    float salary;
-};
